09184353278b9365e0021a1e0b10d2f7cf54eeb3
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-04-10 04:02:18 +0600

    added new code

---
+++ Lecture-01/task2.cpp
@@ -0,0 +1,12 @@
+// Find the area of a square
+#include <iostream>
+using namespace std;
+int main()
+{
+    int r, area;
+    cout << "Enter Radius : " << endl;
+    cin >> r;
+    area = 2 * r * r;
+    cout << "Area of circle = " << area;
+    return 0;
+}
