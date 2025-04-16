edcdefcb0080c8cdcfb8dc225a36b9ecc8021250
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-04-17 02:44:21 +0600

    added new file

---
+++ Lecture-02/task3.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+// primitive data types in c++
+
+int main()
+{
+    // 8 bit = 1 bytes
+    int age = 25;          // 4 bytes
+    char num = 'A';        // 1 bytes
+    float PI = 3.14f;      // 4bytes
+    bool isSafe = true;    // 1 bytes
+    bool notSafe = false;  // 1 bytes
+    double grade = 100.55; // 8 bytes
+    cout << PI << endl;
+    cout << isSafe << " " << notSafe << endl;
+
+    return 0;
+}
