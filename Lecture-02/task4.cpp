d61b6d859ab4059088cfaaebc8875c7bdd8374ad
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-04-17 02:51:36 +0600

    added new code

---
+++ Lecture-02/task4.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+// type casting conversion
+
+int main()
+{
+    char grade = 'a';  // 1 bytes
+    int value = grade; // 4 bytes
+    cout << value << endl;
+    return 0;
+}
