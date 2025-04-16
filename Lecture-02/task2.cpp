c3d595687549df2239374c3a519f2bc725cc14fa
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-04-17 02:25:34 +0600

    added new file

---
+++ Lecture-02/task2.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int age = 25;
+    cout << "i am a programmer and i am" << " " << age << " " << "years old" << endl;
+    return 0;
+}
