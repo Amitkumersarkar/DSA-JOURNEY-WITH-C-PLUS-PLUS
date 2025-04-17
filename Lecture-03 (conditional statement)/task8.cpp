c027df610e17ece9e2069f44b857e1fc2b99214b
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-04-17 22:03:54 +0600

    added new updated code

---
+++ Lecture-03 (conditional statement)/task8.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+// conditional statement
+// print number 1 to 5
+int main()
+{
+    int n = 20;
+    int count = 1;
+    while (count <= n)
+    {
+
+        cout << count << endl;
+        count++;
+    }
+
+    return 0;
+};
