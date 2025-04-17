fa4259fe01d57631ce515c4922737bffe86c5bd8
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-04-17 12:20:03 +0600

    added new file

---
+++ Lecture-02/task7.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+// arithmetic operator
+
+int main()
+{
+    int a = 20, b = 5;
+    int mult = a * b;
+    cout << mult << endl;
+    cout << a + b << endl;
+    cout << a - b << endl;
+    return 0;
+}
