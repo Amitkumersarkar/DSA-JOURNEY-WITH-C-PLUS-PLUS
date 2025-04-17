658ea89de70890b4c1ffcc12228c3eea35576a91
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-04-17 13:06:23 +0600

    added new file

---
+++ Lecture-02/task13.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+// unary operator (a++,++a,a--,--a)
+// post increment operator = (a++)
+// pre increment operator = (++a)
+int main()
+{
+    int a = 20;
+    int b = ++a;       // fist update value then task done
+    cout << b << endl; // 21 (update value and increment 1)
+    cout << a << endl; // 21 (update value and increment 1)
+    return 0;
+}
