04b940eb89fa2cbc0be6744821d6b08a77f7b5dc
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-04-17 19:57:22 +0600

    added new file

---
+++ Lecture-03 (conditional statement)/task1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+// conditional statement
+int main()
+{
+    int n = -50;
+    if (n >= 0)
+    {
+        cout << "N is a positive even number = " << n << endl;
+    }
+    else
+    {
+        cout << "N is not a positive number" << endl;
+    }
+
+    return 0;
+}
