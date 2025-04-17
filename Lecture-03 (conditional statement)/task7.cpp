0e1359be571cdff04549ba24f88d39201be04feb
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-04-17 21:54:38 +0600

    added new file

---
+++ Lecture-03 (conditional statement)/task7.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+// conditional statement
+// print number 1 to 15
+int main()
+{
+    int count = 1;
+    while (count <= 15)
+    {
+        cout << count << endl;
+        count++;
+    }
+
+    return 0;
+};
